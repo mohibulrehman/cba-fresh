c3a0f378fa499c746a176dd2551b80596914b039
mohibraja11 <mohibraja11@gmail.ccom>
2022-04-10 15:26:48 +0100

    task 3 implemented

--- SVM/VirtualMachine/SimpleMachineLanguage/Decr.cs
+++ SVM/VirtualMachine/SimpleMachineLanguage/Decr.cs
@@ -9,9 +9,29 @@
     /// Decrements the integer value stored on top of the stack, 
     /// leaving the result on the stack
     /// </summary>
-    public class Decr
+    public class Decr : BaseInstruction
     {
         #region TASK 3 - TO BE IMPLEMENTED BY THE STUDENT
         #endregion
+        public override void Run()
+        {
+            try
+            {
+                if (VirtualMachine.Stack.Count < 1)
+                {
+                    throw new SvmRuntimeException(String.Format(BaseInstruction.StackUnderflowMessage,
+                                                    this.ToString(), VirtualMachine.ProgramCounter));
+                }
+
+                int op1 = (int)VirtualMachine.Stack.Pop();
+
+                VirtualMachine.Stack.Push(op1 - 1);
+            }
+            catch (InvalidCastException)
+            {
+                throw new SvmRuntimeException(String.Format(BaseInstruction.OperandOfWrongTypeMessage,
+                                                this.ToString(), VirtualMachine.ProgramCounter));
+            }
+        }
     }
 }
--- SVM/VirtualMachine/SimpleMachineLanguage/Incr.cs
+++ SVM/VirtualMachine/SimpleMachineLanguage/Incr.cs
@@ -9,9 +9,33 @@
     /// Increments the integer value stored on top of the stack, 
     /// leaving the result on the stack
     /// </summary>
-    public class Incr
+    public class Incr : BaseInstruction
     {
         #region TASK 3 - TO BE IMPLEMENTED BY THE STUDENT
         #endregion
+
+
+        public override void Run()
+        {
+            try
+            {
+                if (VirtualMachine.Stack.Count < 1)
+                {
+                    throw new SvmRuntimeException(String.Format(BaseInstruction.StackUnderflowMessage,
+                                                    this.ToString(), VirtualMachine.ProgramCounter));
+                }
+
+                int op1 = (int)VirtualMachine.Stack.Pop();
+               // int op2 = (int)VirtualMachine.Stack.Pop();
+                VirtualMachine.Stack.Push(op1 + 1);
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine("InvalidCastException come up ");
+                throw new SvmRuntimeException(String.Format(BaseInstruction.OperandOfWrongTypeMessage,
+                                                this.ToString(), VirtualMachine.ProgramCounter));
+            }
+        }
+     
     }
 }
